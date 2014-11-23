a9cb111615a233a6f9b2505d5b2ac159418f7230
Tony Pitale <tpitale@gmail.com>
2014-11-23 17:54:08 -0500

    Rescue from disconnects during idle, and reconnect Raise/exit on any other error Resolves #11 and #12

--- spec/lib/mailbox_watcher_spec.rb
+++ spec/lib/mailbox_watcher_spec.rb
@@ -118,7 +118,7 @@ describe MailRoom::MailboxWatcher do
 
   describe '#stop_idling' do
     let(:imap) {stub}
-    let(:idling_thread) {stub}
+    let(:idling_thread) {stub(:abort_on_exception=)}
     let(:watcher) {MailRoom::MailboxWatcher.new(nil)}
 
     before :each do
@@ -160,7 +160,7 @@ describe MailRoom::MailboxWatcher do
     let(:watcher) {MailRoom::MailboxWatcher.new(nil)}
 
     before :each do
-      Thread.stubs(:start).yields
+      Thread.stubs(:start).yields.returns(stub(:abort_on_exception=))
       watcher.stubs(:setup)
     end
 
