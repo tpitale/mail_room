7adc6ed17d1137656c2e0ff01bad770614861c74
Tony Pitale <tpitale@gmail.com>
2016-06-17 13:36:07 -0500

    First pass refactoring watcher/handler into a connection, with a simpler watcher class

---
+++ lib/mail_room/connection.rb
@@ -0,0 +1,170 @@
+module MailRoom
+  class Connection
+    def initialize(mailbox)
+      @mailbox = mailbox
+
+      # log in and set the mailbox
+      reset
+      setup
+    end
+
+    def on_new_message(&block)
+      @new_message_handler = block
+    end
+
+    # is the connection logged in?
+    # @return [Boolean]
+    def logged_in?
+      @logged_in
+    end
+
+    # is the connection blocked idling?
+    # @return [Boolean]
+    def idling?
+      @idling
+    end
+
+    # is the imap connection closed?
+    # @return [Boolean]
+    def disconnected?
+      @imap.disconnected?
+    end
+
+    # is the connection ready to idle?
+    # @return [Boolean]
+    def ready_to_idle?
+      logged_in? && !idling?
+    end
+
+    def quit
+      stop_idling
+      reset
+    end
+
+    def wait
+      begin
+        # in case we missed any between idles
+        process_mailbox
+
+        idle
+
+        process_mailbox
+      rescue Net::IMAP::Error, IOError
+        reset
+      end
+    end
+
+    private
+
+    def reset
+      @imap = nil
+      @logged_in = false
+      @idling = false
+    end
+
+    def setup
+      start_tls
+      log_in
+      set_mailbox
+    end
+
+    # build a net/imap connection to google imap
+    def imap
+      @imap ||= MailRoom::IMAP.new(@mailbox.host, :port => @mailbox.port, :ssl => @mailbox.ssl_options)
+    end
+
+    # start a TLS session
+    def start_tls
+      imap.starttls if @mailbox.start_tls
+    end
+
+    # send the imap login command to google
+    def log_in
+      imap.login(@mailbox.email, @mailbox.password)
+      @logged_in = true
+    end
+
+    # select the mailbox name we want to use
+    def set_mailbox
+      imap.select(@mailbox.name) if logged_in?
+    end
+
+    # is the response for a new message?
+    # @param response [Net::IMAP::TaggedResponse] the imap response from idle
+    # @return [Boolean]
+    def message_exists?(response)
+      response.respond_to?(:name) && response.name == 'EXISTS'
+    end
+
+    # @private
+    def idle_handler
+      lambda {|response| imap.idle_done if message_exists?(response)}
+    end
+
+    # maintain an imap idle connection
+    def idle
+      return unless ready_to_idle?
+
+      @idling = true
+
+      imap.idle(@mailbox.idle_timeout, &idle_handler)
+    ensure
+      @idling = false
+    end
+
+    # trigger the idle to finish and wait for the thread to finish
+    def stop_idling
+      return unless idling?
+
+      imap.idle_done
+      
+      # idling_thread.join
+      # self.idling_thread = nil
+    end
+
+    def process_mailbox
+      return unless @new_message_handler
+
+      new_messages.map do |message|
+        [@new_message_handler.call(message), message]
+      end.each do |success, message|
+        scrub(message) if success
+      end
+    end
+
+    def scrub(message)
+      if @mailbox.delete_after_delivery
+        imap.store(message.seqno, "+FLAGS", [Net::IMAP::DELETED])
+      end
+    end
+
+    # @private
+    # fetch all messages for the new message ids
+    def new_messages
+      # Both of these calls may results in
+      #   imap raising an EOFError, we handle
+      #   this exception in the watcher
+      messages_for_ids(new_message_ids)
+    end
+
+    # TODO: label messages?
+    #   @imap.store(id, "+X-GM-LABELS", [label])
+
+    # @private
+    # search for all new (unseen) message ids
+    # @return [Array<Integer>] message ids
+    def new_message_ids
+      @imap.uid_search(@mailbox.search_command).select { |uid| @mailbox.deliver?(uid) }
+    end
+
+    # @private
+    # fetch the email for all given ids in RFC822 format
+    # @param ids [Array<Integer>] list of message ids
+    # @return [Array<Net::IMAP::FetchData>] the net/imap messages for the given ids
+    def messages_for_ids(uids)
+      return [] if uids.empty?
+
+      @imap.uid_fetch(uids, "RFC822")
+    end
+  end
+end
---
+++ spec/lib/connection_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe MailRoom::Connection do
+  let(:imap) {stub}
+  let(:mailbox) {MailRoom::Mailbox.new(delete_after_delivery: true)}
+
+  before :each do
+    MailRoom::IMAP.stubs(:new).returns(imap)
+  end
+
+  context "with imap set up" do
+    let(:connection) {MailRoom::Connection.new(mailbox)}
+
+    before :each do
+      imap.stubs(:starttls)
+      imap.stubs(:login)
+      imap.stubs(:select)
+    end
+
+    it "is logged in" do
+      expect(connection.logged_in?).to eq(true)
+    end
+
+    it "is not idling" do
+      expect(connection.idling?).to eq(false)
+    end
+
+    it "is not disconnected" do
+      imap.stubs(:disconnected?).returns(false)
+
+      expect(connection.disconnected?).to eq(false)
+    end
+
+    it "is ready to idle" do
+      expect(connection.ready_to_idle?).to eq(true)
+    end
+
+    it "waits for a message to process" do
+      new_message = 'a message'
+      new_message.stubs(:seqno).returns(8)
+
+      connection.on_new_message do |message|
+        expect(message).to eq(new_message)
+        true
+      end
+
+      mailbox.stubs(:deliver?).returns(true)
+
+      imap.stubs(:idle)
+      imap.stubs(:uid_search).returns([]).then.returns([1])
+      imap.stubs(:uid_fetch).returns([new_message])
+      imap.stubs(:store)
+
+      connection.wait
+
+      expect(imap).to have_received(:idle)
+      expect(imap).to have_received(:uid_search).with(mailbox.search_command).twice
+      expect(imap).to have_received(:uid_fetch).with([1], "RFC822")
+      expect(mailbox).to have_received(:deliver?).with(1)
+      expect(imap).to have_received(:store).with(8, "+FLAGS", [Net::IMAP::DELETED])
+    end
+  end
+end
